90f9a29eb9596aa7fa7b4e9b1035e47067e5054f
matt <mthw.wm.robinson@gmail.com>
2018-08-27 19:22:13 -0400

    added test for fetching cols

--- server/tests/test_database.py
+++ server/tests/test_database.py
@@ -1,6 +1,11 @@
-from trs_dashboard.database.database import Database
+from trs_dashboard.etl.database import Database
 
 def test_initialize():
     database = Database()
     database.initialize()
     assert database.connection.status == 1
+
+def test_get_columns():
+    database = Database()
+    columns = database.get_columns('events')
+    assert len(columns) > 0

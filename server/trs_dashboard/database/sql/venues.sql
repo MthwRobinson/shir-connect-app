a69fadeb57dbfbf47e71993933bfb1e5e5096616
matt <mthw.wm.robinson@gmail.com>
2018-09-01 09:16:12 -0400

    added ddl for venues

---
+++ server/trs_dashboard/database/sql/venues.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS {schema}.venues (
+  id text,
+  address_1 text,
+  address_2 text,
+  city text,
+  region text,
+  country text,
+  latitude numeric,
+  longitude numeric,
+  postal_code text,
+  name text
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS venue_index ON {schema}.venues (id);

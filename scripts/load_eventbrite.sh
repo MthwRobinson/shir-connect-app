785b8ce2a729ef036f88bb6297d004d9a9521a79
root <root@ip-172-31-27-219.us-east-2.compute.internal>
2018-09-05 02:32:56 +0000

    added script to run eventbrite load

---
+++ scripts/load_eventbrite.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+LOCK=/var/tmp/eventbrite_load
+if [ -f $LOCK ]; then
+  echo Eventbrite load is already running\!
+  exit 6
+fi
+touch $LOCK
+source /home/trs_dashboard/.bashrc
+/home/trs_dashboard/anaconda3/bin/trs_dashboard load_eventbrite
+rm $LOCK

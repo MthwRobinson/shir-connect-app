932c6e33c2f6350cd92b0b32b530ef6bff8c7f95
matt <mthw.wm.robinson@gmail.com>
2018-09-20 21:13:45 -0400

    added react forms to login

--- ui/src/components/Login/Login.js
+++ ui/src/components/Login/Login.js
@@ -7,22 +7,49 @@ import {
   FormControl, 
   FormGroup 
 } from 'react-bootstrap';
+import { Field, reduxForm } from 'redux-form';
 
 import './Login.css';
 
+
 class Login extends Component {
     render() {
+      const userName = () => {
+        return(
+          <div>
+            <ControlLabel>User Name</ControlLabel>
+            <FormControl type="text"></FormControl>
+          </div>
+
+        )
+      }
+
+      const password = () => {
+        return(
+          <div>
+            <ControlLabel>Password</ControlLabel>
+            <FormControl type="text"></FormControl>
+          </div>
+        )
+      }
+
       return (
         <div className="Login pullLeft">
           <h2>Login</h2>
           <Form horizontal>
             <FormGroup className="pullLeft">
-              <ControlLabel>User Name</ControlLabel>
-              <FormControl type="text"></FormControl>
+              <Field
+                name="userName"
+                type="text"
+                component={userName}
+              />
             </FormGroup>
             <FormGroup>
-              <ControlLabel>Password</ControlLabel>
-              <FormControl type="text"></FormControl>
+              <Field
+                name="password"
+                type="text"
+                component={password}
+              />
             </FormGroup>
             <Button className="login-button"  bsStyle="primary">Submit</Button>
           </Form>
@@ -31,4 +58,4 @@ class Login extends Component {
     }
 }
 
-export default Login;
+export default reduxForm({form: 'login'})(Login);

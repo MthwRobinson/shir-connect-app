f156a63f625ac4e9d6494990d47101655d7fedff
matt <mthw.wm.robinson@gmail.com>
2018-09-22 11:42:33 -0400

    removed redux folders

--- ui/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// Combines all the reducers
-// Reducers are imported into App.js
-import { combineReducers } from 'redux';
-import { reducer as form } from 'redux-form'
-
-export default combineReducers({
-  form
-});

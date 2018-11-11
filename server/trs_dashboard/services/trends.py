4ebf891557cdeddef16c894899cb23c347c76acc
matt <mthw.wm.robinson@gmail.com>
2018-11-11 17:11:57 -0500

    added service for age groups

--- server/trs_dashboard/services/trends.py
+++ server/trs_dashboard/services/trends.py
@@ -33,6 +33,14 @@ def average_attendance():
     response = trends.get_average_attendance()
     return jsonify(response)
 
+@trends.route('/service/trends/age-group-attendees')
+@jwt_required
+def age_group_attendees():
+    """ Finds a distinct count of attendees by age group and year """
+    trends = Trends()
+    response = trends.get_age_group_attendees()
+    return jsonify(response)
+
 class Trends(object):
     """ Class that handles database calls for trends """
     def __init__(self):
@@ -85,4 +93,50 @@ class Trends(object):
             ORDER BY day_order ASC
         """.format(schema=self.database.schema)
         response = self.database.fetch_list(sql)
+        return response
+
+    def get_age_group_attendees(self):
+        """ Returns a count of unique attendees by age group and year """
+        sql = """
+            SELECT
+                CASE
+                    WHEN age < 13 THEN 'Under 13'
+                    WHEN age >= 13 AND age < 18 THEN 'Teens'
+                    WHEN age >= 18 AND age < 23 THEN 'College'
+                    WHEN age >= 23 AND age < 40 THEN 'Young Professional'
+                    WHEN age >= 40 AND age < 50 THEN '40-50'
+                    WHEN age >= 50 AND age < 60 THEN '50-60'
+                    WHEN age >= 60 AND age < 70 THEN '60-70'
+                    WHEN age >= 70 AND age < 80 THEN '70-80'
+                    WHEN age >= 80 THEN 'Over 80'
+                    ELSE 'Unknown'
+                END AS age_group,
+                event_year,
+                COUNT(DISTINCT attendee_id) as distinct_attendees
+            FROM(
+                SELECT DISTINCT
+                    event_id,
+                    b.id as member_id,
+                    a.id as attendee_id,
+                    date_part('year', start_datetime) as event_year,
+                    date_part('year', start_datetime) - date_part('year', birth_date) as age
+                FROM {schema}.attendees a
+                INNER JOIN {schema}.members_view b
+                ON (LOWER(a.first_name)=LOWER(b.first_name) 
+                AND LOWER(a.last_name)=LOWER(b.last_name))
+                INNER JOIN {schema}.event_aggregates c
+                ON a.event_id = c.id
+            ) x
+            GROUP BY event_year, age_group
+            ORDER BY event_year ASC, age_group DESC
+        """.format(schema=self.database.schema)
+        list_response = self.database.fetch_list(sql)
+
+        response = {}
+        for row in list_response['results']:
+            age_group = row['age_group']
+            if age_group not in response:
+                response[age_group] = {'year': [], 'count': []}
+            response[age_group]['year'].append(row['event_year'])
+            response[age_group]['count'].append(row['distinct_attendees'])
         return response

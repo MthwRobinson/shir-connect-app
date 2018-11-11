bd543d15521cbe7feba21b21b9779c3bad92fc92
matt <mthw.wm.robinson@gmail.com>
2018-11-11 15:19:19 -0500

    refactored plots into their own components

---
+++ ui/src/components/Trends/Plots/AverageAttendancePlot.js
@@ -0,0 +1,86 @@
+// Reners the average attendance component
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+import Plot from 'react-plotly.js';
+
+import Loading from './../../Loading/Loading';
+
+class AverageAttendancePlot extends Component {
+  // Class displaying the average attendance by day
+  state = {
+    x: [],
+    y: [],
+    loading: true
+  }
+
+  componentDidMount(){
+    this.getAverageAttendance();
+  }
+
+  getAverageAttendance = () => {
+    this.setState({loading: true});
+    const token = localStorage.getItem('trsToken');
+    const auth = 'Bearer '.concat(token)
+    let url = '/service/trends/avg-attendance';
+    axios.get(url, { headers: { Authorization: auth }})
+      .then(res => {
+        let x = [];
+        let y = [];
+
+        for(var i=0; i<res.data.results.length; i++){
+          let data = res.data.results[i];
+          y.push(data.avg_attendance);
+          x.push(data.day_of_week);
+        }
+
+        this.setState({
+          x: x,
+          y: y,
+          loading: false
+        })
+      })
+      .catch(err => {
+        if(err.response.status===401){
+          this.props.history.push('/login');
+        }
+      })
+  }
+
+  render(){
+    if(this.state.loading){
+      return (
+        <div className='event-loading'>
+          <Loading />
+        </div>
+      )
+    } else {
+      return (
+        <div className='plot-area'>
+          <Plot
+            data={[{
+              x: this.state.x,
+              y: this.state.y,
+              type: 'bar',
+              marker: {color: '#0038b8'},
+            }]}
+            layout={ {
+              width: 1200,
+              height: 470,
+              title: 'Average Attendance by Day',
+              titlefont: {family: 'Source Sans Pro'},
+              yaxis: {
+                title: 'Average Attendance',
+                titlefont: {family: 'Source Sans Pro'}
+              },
+              xaxis: { tickangle: 45 }
+            }
+            }
+          />
+        </div>
+      )
+    }
+  }
+}
+
+export default withRouter(AverageAttendancePlot);
---
+++ ui/src/components/Trends/Plots/MonthlyRevenuePlot.js
@@ -0,0 +1,93 @@
+// Renders the monthly revenue component
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import axios from 'axios';
+import Plot from 'react-plotly.js';
+
+import Loading from './../../Loading/Loading';
+
+class MonthlyRevenuePlot extends Component {
+  // Class displaying the monthly revenue plot
+  state = {
+    x: [],
+    y: [],
+    loading: true
+  }
+
+  componentDidMount(){
+    this.getRevenue();
+  }
+
+  getRevenue = () => {
+    this.setState({loading: true});
+    const token = localStorage.getItem('trsToken');
+    const auth = 'Bearer '.concat(token)
+    let url = '/service/trends/monthly-revenue';
+    axios.get(url, { headers: { Authorization: auth }})
+      .then(res => {
+        let x = [];
+        let y = [];
+
+        for(var i=0; i<res.data.results.length; i++){
+          let data = res.data.results[i];
+          y.push(data.revenue);
+          let month = data.mn + '/' 
+          month += data.yr;
+          x.push(month);
+        }
+
+        this.setState({
+          x: x,
+          y: y,
+          loading: false
+        })
+      })
+      .catch(err => {
+        if(err.response.status===401){
+          this.props.history.push('/login');
+        }
+      })
+  }
+
+  render(){
+    if(this.state.loading){
+      return (
+        <div className='event-loading'>
+          <Loading />
+        </div>
+      )
+    } else {
+      return (
+        <div className='plot-area'>
+          <Plot
+            data={[{
+            x: this.state.x,
+              y: this.state.y,
+              type: 'scatter',
+              mode: 'lines+points',
+              marker: {color: '#0038b8'},
+            }]}
+            layout={ {
+              width: 1200,
+              height: 470,
+              title: 'Event Revenue by Month',
+              titlefont: {family: 'Source Sans Pro'},
+              yaxis: {
+                title: 'Revenue (in dollars)',
+                titlefont: {family: 'Source Sans Pro'}
+              },
+              xaxis: {
+                title: 'Month',
+                titlefont: {family: 'Source Sans Pro'},
+                tickangle: 45
+              }
+            }
+            }
+          />
+        </div>
+      )
+    }
+  }
+}
+
+export default withRouter(MonthlyRevenuePlot);

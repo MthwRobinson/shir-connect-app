37d2e1c36034d29fb615b68c40f1417725a10dbc
matt <mthw.wm.robinson@gmail.com>
2018-12-16 14:00:14 -0500

    added loading icon to home screen

--- ui/src/components/Home/Home.js
+++ ui/src/components/Home/Home.js
@@ -5,6 +5,7 @@ import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 
 import ModuleCard from './../ModuleCard/ModuleCard';
+import Loading from './../Loading/Loading';
 
 import './Home.css';
 
@@ -12,7 +13,8 @@ const MODULES = require('./../../data/modules.json');
 
 class Home extends Component {
     state = {
-      name: ''
+      name: '',
+      loading: true
     }
 
 
@@ -31,7 +33,10 @@ class Home extends Component {
         const auth = 'Bearer '.concat(token);
         axios.get('/service/test', { headers: { Authorization: auth }})
           .then(res => {
-            this.setState({name: res.data.name});
+            this.setState({
+              name: res.data.name,
+              loading: false
+            });
           })
           .catch( err => {
             if(err.response.status===401){
@@ -42,50 +47,58 @@ class Home extends Component {
     }
 
     render() {
-      return (
-        <div className="Home">
-          <div className="home-header">
-            <h2>Welcome, {this.state.name}!</h2><hr/>
-            <h4>Choose a module to begin.</h4>
+      if(this.state.loading){
+        return(
+          <div className="event-loading">
+            <Loading />
           </div>
-          <Row className="module-row">
-            <Col xs={12} sm={12} md={6} lg={6}>
-              <ModuleCard 
-                title={MODULES[0].title}
-                icon={MODULES[0].icon}
-                bullets={MODULES[0].bullets}
-                click={()=>this.navigate(MODULES[0].link)}
-              />
-            </Col>
-            <Col xs={12} sm={12} md={6} lg={6}>
-              <ModuleCard 
-                title={MODULES[1].title}
-                icon={MODULES[1].icon}
-                bullets={MODULES[1].bullets}
-                click={()=>this.navigate(MODULES[1].link)}
-              />
-            </Col>
-          </Row>
-          <Row className="module-row">
-            <Col xs={12} sm={12} md={6} lg={6}>
-              <ModuleCard 
-                title={MODULES[2].title}
-                icon={MODULES[2].icon}
-                bullets={MODULES[2].bullets}
-                click={()=>this.navigate(MODULES[2].link)}
-              />
-            </Col>
-            <Col xs={12} sm={12} md={6} lg={6}>
-              <ModuleCard 
-                title={MODULES[3].title}
-                icon={MODULES[3].icon}
-                bullets={MODULES[3].bullets}
-                click={()=>this.navigate(MODULES[3].link)}
-              />
-            </Col>
-          </Row>
-        </div>
-      );
+        )
+      } else {
+        return (
+          <div className="Home">
+            <div className="home-header">
+              <h2>Welcome, {this.state.name}!</h2><hr/>
+              <h4>Choose a module to begin.</h4>
+            </div>
+            <Row className="module-row">
+              <Col xs={12} sm={12} md={6} lg={6}>
+                <ModuleCard 
+                  title={MODULES[0].title}
+                  icon={MODULES[0].icon}
+                  bullets={MODULES[0].bullets}
+                  click={()=>this.navigate(MODULES[0].link)}
+                />
+              </Col>
+              <Col xs={12} sm={12} md={6} lg={6}>
+                <ModuleCard 
+                  title={MODULES[1].title}
+                  icon={MODULES[1].icon}
+                  bullets={MODULES[1].bullets}
+                  click={()=>this.navigate(MODULES[1].link)}
+                />
+              </Col>
+            </Row>
+            <Row className="module-row">
+              <Col xs={12} sm={12} md={6} lg={6}>
+                <ModuleCard 
+                  title={MODULES[2].title}
+                  icon={MODULES[2].icon}
+                  bullets={MODULES[2].bullets}
+                  click={()=>this.navigate(MODULES[2].link)}
+                />
+              </Col>
+              <Col xs={12} sm={12} md={6} lg={6}>
+                <ModuleCard 
+                  title={MODULES[3].title}
+                  icon={MODULES[3].icon}
+                  bullets={MODULES[3].bullets}
+                  click={()=>this.navigate(MODULES[3].link)}
+                />
+              </Col>
+            </Row>
+          </div>
+        );
+      }
     }
 }
 

7bdb5256af2b44b3bbe7badb138e87ad06327c31
matt <mthw.wm.robinson@gmail.com>
2019-03-01 21:24:56 -0500

    Removed auth headers from trends

--- ui/src/components/Trends/Trends.js
+++ ui/src/components/Trends/Trends.js
@@ -3,10 +3,7 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-import {
-  getAccessToken,
-  refreshAccessToken
-} from './../../utilities/authentication';
+import { refreshAccessToken } from './../../utilities/authentication';
 import AgeGroupAttendance from './../AgeGroupAttendance/AgeGroupAttendance';
 import Header from './../Header/Header';
 
@@ -20,10 +17,8 @@ class Trends extends Component {
   componentDidMount(){
     // Checks to make sure the user has access to the 
     // trends access group
-    const token = getAccessToken();
-    const auth = 'Bearer '.concat(token);
     const url = '/service/trends/authorize';
-    axios.get(url, {headers: {Authorization: auth }})
+    axios.get(url)
       .then(res => {
         // Refresh the token to keep the session active
         refreshAccessToken();

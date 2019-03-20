e82a24c236ad86815420120700fd98d4409bad28
matt <mthw.wm.robinson@gmail.com>
2019-03-19 22:40:46 -0400

    Refactored get_config into utils

--- server/shir_connect/configuration.py
+++ server/shir_connect/configuration.py
@@ -1,9 +1,7 @@
 """ Configurations file for the REST platfrom and ETL pipeline """
-import datetime
 import os
 
-import requests
-import yaml
+from shir_connect.utils import get_config
 
 
 # Application paths
@@ -56,44 +54,7 @@ MAP_GROUP = 'map'
 ACCESS_GROUPS = [EVENT_GROUP, MEMBER_GROUP, TRENDS_GROUP, MAP_GROUP]
 
 # Custom Configurations
-config = get_config()
+config = get_config(PROJPATH, HOMEPATH)
 AGE_GROUPS = config['age_groups']
 DEFAULT_LOCATION = config['location']
 IDENTIFIERS = config['identifiers']
-
-def get_config():
-    """ Pulls settings from the configs folder with the following
-    order of precedence:
-    1. If a config file lists the IP address of the server, us that config
-    2. Otherwise, use the config that is marked as the default
-    """
-    custom_config = {}
-    config_path = os.path.join(PROJPATH, 'configs')
-    config_files = os.listdir(config_path)
-    ip_addr = get_ip()
-    for file_ in config_files:
-        filename = config_path + '/' + file_
-        with open(filename, 'r') as f:
-            config = yaml.safe_load(f)
-        if ip_addr in config['ip_addresses']:
-            custom_config = config
-            break
-        if config['default']:
-            default_config = config
-    if not custom_config:
-        custom_config = default_config
-    return custom_config
-
-def get_ip():
-    """ Determines the IP address of the server. """
-    files = os.listdir(HOMEPATH)
-    ip_file = '.shir-connect-ip'
-    filename = os.path.join(HOMEPATH, ip_file)
-    if ip_file in files:
-        with open(filename, 'r') as f:
-            ip_addr = f.readline()
-    else:
-        ip_addr = requests.get('https://api.ipify.org').text
-        with open(filename, 'w') as f:
-            f.write(ip_addr)
-    return ip_addr

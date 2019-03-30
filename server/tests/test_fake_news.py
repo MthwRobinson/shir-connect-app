f0fa001464e4422b3044f73049d731fce6721034
matt <mthw.wm.robinson@gmail.com>
2019-03-30 15:05:51 -0400

    added test for fake people data

--- server/tests/test_fake_news.py
+++ server/tests/test_fake_news.py
@@ -5,7 +5,17 @@ from shir_connect.database.fake_news import FakeNews
 
 @pytest.fixture
 def fake_news():
+    def read_table(name):
+        if name in ['participants', 'attendees', 'members', 'orders']:
+            return pd.DataFrame({'first_name': ['Jabber', 'Chester',
+                                                'Missy', 'Cody', 
+                                                'Jabber', 'Shenzy'],
+                                 'last_name': ['Robinson', 'Shindhelm',
+                                               'Robinson', 'Barky',
+                                               'Robinson', 'Squawky']})
+
     fake_news = FakeNews()
+    fake_news.database.read_table = lambda x: read_table(x)
     fake_news.database.update_column = lambda *args, **kwargs: 'Squawk!'
     return fake_news
 
@@ -16,3 +26,11 @@ def test_find_name(fake_news):
                                      'Shindhelm', 'Robinson']})
     subset = fake_news._find_name('Jabber', 'Robinson', df)
     assert len(subset) == 2
+
+def test_get_person_tables(fake_news):
+    fake_news._get_person_tables()
+    tables = ['participants', 'attendees', 'members', 'order']
+    for table in tables:
+        df = getattr(fake_news, table)
+        assert isinstance(df, pd.core.frame.DataFrame)
+        assert len(df) == 6

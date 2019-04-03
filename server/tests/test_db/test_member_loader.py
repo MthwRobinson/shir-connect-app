2bcc6b124f832faaea9eb4cf6c71fa838fd2926d
matt <mthw.wm.robinson@gmail.com>
2019-04-02 23:07:00 -0400

    split tests into folders

--- server/tests/test_db/test_member_loader.py
+++ server/tests/test_db/test_member_loader.py
@@ -7,7 +7,7 @@ from shir_connect.database.member_loader import MemberLoader
 PATH = os.path.dirname(os.path.realpath(__file__))
 
 def test_mm2000():
-    filename = PATH + '/data/mm2000_upload_test.csv'
+    filename = os.path.join(PATH, '..', 'data/mm2000_upload_test.csv')
     df = pd.read_csv(filename, encoding='latin-1')
     member_loader = MemberLoader()
     items = member_loader.parse_mm2000(df)

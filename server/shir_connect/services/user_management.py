e8f3e231e6415d94b9a9c044403d4e7cd1abacca
matt <mthw.wm.robinson@gmail.com>
2019-04-02 22:18:36 -0400

    refactored user_managemet into its own module

--- server/shir_connect/services/user_management.py
+++ server/shir_connect/services/user_management.py
@@ -1,16 +1,11 @@
-""" 
+"""
 REST endpoints for operations pertaining to users
 A user must be authenticated to register a new user
 Includes:
     1. Flask routes with /user path
     2. UserManagment class for database calls
 """
-import hashlib
-import logging
-import random
-
-import daiquiri
-from flask import Blueprint, abort, jsonify, request
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
@@ -21,12 +16,9 @@ from flask_jwt_extended import (
     set_refresh_cookies,
     unset_jwt_cookies
 )
-import pandas as pd
-import numpy as np
-import xkcdpass.xkcd_password as xkcd
 
 import shir_connect.configuration as conf
-from shir_connect.database.database import Database
+from shir_connect.database.user_management import UserManagement
 
 user_management = Blueprint('user_management', __name__)
 
@@ -63,7 +55,7 @@ def user_register():
     new_user['password'] = user_management.generate_password()
 
     status = user_management.add_user(
-        username=new_user['username'], 
+        username=new_user['username'],
         password=new_user['password'],
         role=new_user['role'],
         modules=new_user['modules']
@@ -95,7 +87,7 @@ def delete_user(username):
 
 @user_management.route('/service/user/authenticate', methods=['POST'])
 def user_authenticate():
-    """ 
+    """
     Authenticates a user and returns a json web token
     In the response body, refresh_token is the refresh token
         and jwt is the access token
@@ -121,7 +113,7 @@ def user_authenticate():
         access_token = create_access_token(identity=username)
         refresh_token = create_refresh_token(identity=username)
         response = jsonify({'login': True})
-        set_access_cookies(response, access_token, 
+        set_access_cookies(response, access_token,
                            max_age=conf.JWT_ACCESS_TOKEN_EXPIRES)
         set_refresh_cookies(response, refresh_token,
                             max_age=conf.JWT_REFRESH_TOKEN_EXPIRES)
@@ -277,7 +269,7 @@ def reset_password():
         if 'username' not in request.json:
             response = {'message': 'username required in post body'}
             return jsonify(response), 400
-        
+
         # Generate a password and post the update to the datase
         username = request.json['username']
         password = user_management.generate_password()
@@ -301,161 +293,3 @@ def list_users():
     else:
         users = user_management.list_users()
         return jsonify(users), 200
-
-class UserManagement(object):
-    """ Class that handles user centric REST operations """
-    def __init__(self):
-        daiquiri.setup(level=logging.INFO)
-        self.logger = daiquiri.getLogger(__name__)
-
-        self.database = Database()
-
-        self.access_groups = conf.ACCESS_GROUPS
-        self.user_roles = conf.USER_ROLES
-
-    def get_user(self, username):
-        """ Fetches a user from the database """
-        return self.database.get_item('users', username)
-
-    def authenticate_user(self, username, password):
-        """
-        Checks the password of the user
-        Returns True if the user is authorized
-        """
-        user = self.get_user(username)
-        if not user:
-            return False
-        
-        pw_hash = self.hash_pw(password)
-        if user['password'] == pw_hash:
-            return True
-        else:
-            return False
-
-    def add_user(self, username, password, 
-                 role='standard', modules=[]):
-        """ Adds a new user to the database """
-        complex_enough = self.check_pw_complexity(password)
-        if not complex_enough:
-            return False
-
-        # Check to see if the user already exists
-        user = self.get_user(username)
-        if user:
-            return False
-        else:
-            pw_hash = self.hash_pw(password)
-            item = {
-                'id': username,
-                'password': pw_hash,
-                'role': role,
-                'modules': modules
-            }
-            self.database.load_item(item, 'users')
-            return True
-
-    def delete_user(self, username):
-        """ Deletes a user from the database """
-        self.database.delete_item('users', username)
-
-    def hash_pw(self, password):
-        """ Hashes a password """
-        pw = password.encode('utf-8')
-        pw_hash = hashlib.sha512(pw).hexdigest()
-        return pw_hash
-
-    def update_password(self, username, password):
-        """ Updates the password for a user. """
-        complex_enough = self.check_pw_complexity(password)
-        if not complex_enough:
-            return False
-
-        # Check to see if the user exists
-        user = self.get_user(username)
-        if user:
-            pw_hash = self.hash_pw(password)
-            pw_value = "'%s'"%(pw_hash)
-            self.database.update_column(
-                table='users',
-                item_id=username,
-                column='password',
-                value=pw_value
-            )
-            return True
-        else:
-            return False
-
-    def update_role(self, username, role):
-        """ 
-        Updates the role for the user.
-        The two types of users are admin and standard
-        """
-        if role not in self.user_roles:
-            return False
-        else:
-            value = "'%s'"%(role)
-            self.database.update_column(
-                table='users',
-                item_id=username,
-                column='role',
-                value=value
-            )
-            return True
-
-    def update_access(self, username, modules):
-        """
-        Updates the modules thate are available for the user.
-        The available types are events, members, trends and map
-        """
-        mods = [x for x in modules if x in self.access_groups]
-        value = "'{" + ','.join(['"%s"'%(x) for x in mods]) + "}'"
-        self.database.update_column(
-            table='users',
-            item_id=username,
-            column='modules',
-            value=value
-        )
-
-    def list_users(self):
-        """ Lists all of the active users """
-        df = self.database.read_table('users', sort='id', order='asc')
-        users = []
-        for i in df.index:
-            user = dict(df.loc[i])
-            del user['password']
-            users.append(user)
-        return users
-
-    def check_pw_complexity(self, password):
-        """ Checks to ensure a password is sufficiently complex """
-        if len(password) < 10:
-            return False
-        elif password.isalnum():
-            return False
-        elif password.islower():
-            return False
-        elif password.isupper():
-            return False
-        else:
-            return True
-
-    def generate_password(self):
-        """ Creates an XKCD style password """
-        # Choose four random english words and three random integers
-        wordfile = xkcd.locate_wordfile()
-        wordlist = xkcd.generate_wordlist(
-            wordfile=wordfile,
-            min_length=4,
-            max_length=8
-        )
-        words = xkcd.generate_xkcdpassword(wordlist, numwords=4).split()
-        numbers = np.random.randint(low=0, high=9, size=3)
-        
-        # Generate the password
-        password = ''
-        for i, word in enumerate(words):
-            password += word.title()
-            if i < 3:
-                password += str(numbers[i])
-        password += random.choice('!@#$%&')
-        return password
--- server/tests/test_user_management.py
+++ server/tests/test_user_management.py
@@ -1,5 +1,5 @@
 from shir_connect.services.app import app
-from shir_connect.services.user_management import UserManagement
+from shir_connect.database.user_management import UserManagement
 import shir_connect.services.utils as utils
 
 CLIENT = app.test_client()

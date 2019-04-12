164e046acff87082b37f1417b719f1db4a8abefc
matt <mthw.wm.robinson@gmail.com>
2019-04-11 21:38:17 -0400

    added test for check columns

--- server/tests/test_db/test_member_loader.py
+++ server/tests/test_db/test_member_loader.py
@@ -6,6 +6,13 @@ from shir_connect.database.member_loader import MemberLoader
 
 PATH = os.path.dirname(os.path.realpath(__file__))
 
+class FakeDatabase:
+    def __init__(self):
+        pass
+
+    def get_columns(self, table):
+        return ['parrots', 'dogs']
+
 def test_mm2000():
     filename = os.path.join(PATH, '..', 'data/mm2000_upload_test.csv')
     df = pd.read_csv(filename, encoding='latin-1')
@@ -15,3 +22,18 @@ def test_mm2000():
         assert 'id' in item
         assert 'first_name' in item
         assert 'last_name' in item
+
+def test_check_columns():
+    class OtherFakeDB(FakeDatabase):
+        def get_columns(self, table):
+            if table == 'members':
+                return ['koalas']
+            else:
+                return ['parrots', 'dogs']
+
+    member_loader = MemberLoader()
+    member_loader.database = FakeDatabase()
+    assert member_loader.check_columns() is True
+
+    member_loader.database = OtherFakeDB()
+    assert member_loader.check_columns() is False

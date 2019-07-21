153257f17fa98ae96035092c6f6e04dd6e4fff33
matt <mthw.wm.robinson@gmail.com>
2019-07-20 22:56:59 -0400

    moved dataloader object to eventbrite file

--- server/shir_connect/etl/eventbrite.py
+++ server/shir_connect/etl/eventbrite.py
@@ -1,12 +1,22 @@
-""" Connects to the Eventbrite API """
+"""
+Objects for pulling and loading data from Postgres. Includes:
+    1. Eventbrite: The class for making calls to the Eventbrite API
+    2. EventbriteLoader: The class for making Eventbrite calls, transforming
+        it appropriately and loading it into the Postgres database
+"""
+from copy import deepcopy
+import datetime
 import json
 import logging
+import time
 import urllib
 
+import arrow
 import daiquiri
 import requests
 
 import shir_connect.configuration as conf
+from shir_connect.database.database import Database
 
 class Eventbrite(object):
     """ Makes Eventbrite REST calls using an OAUTH token """
@@ -118,3 +128,198 @@ class Eventbrite(object):
         else:
             venue = json.loads(response.text)
             return venue
+
+class EventbriteLoader(object):
+    """Loads data from Eventbrite into Postgres """
+    def __init__(self):
+        daiquiri.setup(level=logging.INFO)
+        self.logger = daiquiri.getLogger(__name__)
+
+        self.database = Database()
+        self.eventbrite = Eventbrite()
+        self.eventbrite_org = conf.IDENTIFIERS['eventbrite_org']
+
+    def run(self, test=False):
+        """ Runs the data load process """
+        last_event_date = self.database.last_event_date()
+        if last_event_date:
+            look_back = datetime.datetime.now() - datetime.timedelta(days=60)
+            first_event = min(look_back, last_event_date)
+            start = str(first_event)[:10]
+            self.logger.info('Loading events starting at %s'%(start))
+        else:
+            self.logger.info('Loading events from the first available event')
+            start = None
+        events = self.get_events(start=start, page=1)
+
+        num_events = events['pagination']['object_count']
+        if num_events > 0:
+            self.logger.info('There are %s events to process'%(num_events))
+        else:
+            self.logger.info('There are not next events. Exiting')
+            return
+
+        more_events = True
+        while more_events:
+            for event in events['events']:
+                if not event:
+                    continue
+                msg = "Loading information for %s"%(event['name']['text'])
+                self.logger.info(msg)
+                # Load the event into the database. Delete the current
+                # entry in order to maintain the unique index
+                event_id = event['id']
+                if not test:
+                    self.database.delete_item('events', event_id)
+                    self.load_event(event)
+
+                # Load the venue, if it does not already
+                # appear in the database
+                venue_id = event['venue_id']
+                venue_ = self.database.get_item('venues', venue_id)
+                if venue_id and not venue_:
+                    venue = self.get_venue(venue_id)
+                    if not test:
+                        self.load_venue(venue)
+
+                attendees = self.get_attendees(event_id, page=1)
+                more_attendees = True
+                while more_attendees:
+                    if not attendees:
+                        break
+                    for attendee in attendees['attendees']:
+                        if not attendee:
+                            continue
+                        if not test:
+                            self.database.delete_item(
+                                'attendees',
+                                attendee['id'],
+                                {'event_id': event_id}
+                            )
+                            self.load_attendee(attendee)
+
+                    if test or not attendees['pagination']['has_more_items']:
+                        more_attendees = False
+                        break
+                    else:
+                        page = attendees['pagination']['page_number'] + 1
+                        attendees = self.get_attendees(event_id, page)
+                # Sleep to avoid the Eventbrite rate limit
+                if test:
+                    return
+                else:
+                    time.sleep(60)
+
+            if not events['pagination']['has_more_items']:
+                more_events = False
+                break
+            else:
+                page = events['pagination']['page_number'] + 1
+                msg = 'Pulling events on page %s'%(page)
+                self.logger.info(msg)
+                events = self.get_events(start, page)
+
+        # Refresh the materialized views with the new data
+        self.database.refresh_views()
+
+    def get_events(self, start, page=1):
+        """ 
+        Pulls events from eventbrite and sleeps if the rate limit
+        has been exceeded
+        """
+        org_id = self.eventbrite_org
+        events = self.eventbrite.get_events(org_id=org_id, start=start, 
+                                            page=page)
+        if not events:
+            # Sleep until eventbrite resets
+            self.logger.info('Rate limit exceed. Sleeping 30 mins')
+            time.sleep(3600)
+            events = self.eventbrite.get_events(start=start, page=page)
+        return events
+
+    def get_attendees(self, event_id, page=1):
+        """
+        Pulls attendees from eventbrite and sleeps if the rate limit
+        has been exceeded
+        """
+        attendees = self.eventbrite.get_attendees(event_id, page)
+        if not attendees:
+            # If events comes back as none, sleep until the 
+            # Eventbrite rate limit resets
+            self.logger.info('Rate limit exceed. Sleeping 30 mins')
+            time.sleep(3600)
+            attendees = self.eventbrite.get_attendees(event_id, page)
+        return attendees
+
+    def get_venue(self, venue_id, page=1):
+        """
+        Pull a venue and sleeps if the rate limit
+        has been exceeded
+        """
+        venue = self.eventbrite.get_venue(venue_id, page)
+        if not venue:
+            self.logger.info('Rate limit exceed. Sleeping 30 mins')
+            time.sleep(3600)
+            venue = self.eventbrite.get_venue(event_id, page)
+        return venue
+
+    def load_event(self, event):
+        """ Loads an event into the database """
+        event_ = deepcopy(event)
+
+        start = arrow.get(event_['start']['utc']).datetime
+        event_['start_datetime'] = start
+
+        end = arrow.get(event_['end']['utc']).datetime
+        event_['end_datetime'] = end
+
+        description = event_['description']['text']
+        event_['description'] = description
+
+        name = event_['name']['text']
+        event_['name'] = name
+
+        event_['load_datetime'] = datetime.datetime.utcnow()
+        self.database.load_item(event_, 'events')
+
+    def load_attendee(self, attendee):
+        """ Loads an attendee into the database """
+        attendee_ = deepcopy(attendee)
+
+        profile = attendee_['profile']
+        if 'name' in profile:
+            attendee_['name'] = profile['name']
+        if 'first_name' in profile:
+            attendee_['first_name'] = profile['first_name']
+        if 'last_name' in profile:
+            attendee_['last_name'] = profile['last_name']
+        if 'email' in profile:
+            attendee_['email'] = profile['email']
+
+        cost = attendee_['costs']['gross']['major_value']
+        attendee_['cost'] = float(cost)
+
+        attendee_['load_datetime'] = datetime.datetime.utcnow()
+        self.database.load_item(attendee_, 'attendees')
+
+    def load_order(self, order):
+        """ Loads an order into the database """
+        order_ = deepcopy(order)
+
+        cost = order_['costs']['gross']['major_value']
+        order_['cost'] = float(cost)
+
+        order_['load_datetime'] = datetime.datetime.utcnow()
+        self.database.load_item(order_, 'orders')
+
+    def load_venue(self, venue):
+        """ Loads a venue into the database """
+        venue_ = deepcopy(venue)
+
+        for key in venue_['address']:
+            val = venue_['address'][key]
+            venue_[key] = val
+
+        venue_['latitude'] = float(venue_['latitude'])
+        venue_['longitude'] = float(venue_['longitude'])
+        self.database.load_item(venue_, 'venues')
--- server/tests/test_etl/test_eventbrite.py
+++ server/tests/test_etl/test_eventbrite.py
@@ -1,33 +1,88 @@
-from shir_connect.etl.eventbrite import Eventbrite
+import datetime
 
-def test_token():
+from shir_connect.etl.eventbrite import Eventbrite, EventbriteLoader
+
+def test_eventbrite_token():
     eventbrite = Eventbrite()
     response = eventbrite.get_token_info()
     assert response.status_code == 200
 
-def test_events():
+def test_eventbrite_events():
     eventbrite = Eventbrite()
     events = eventbrite.get_events(1358538665, start='2018-07-01')
     assert type(events['events']) == list
     event = events['events'][0]
     assert event['start']['local'] >= '2018-07-01'
 
-def test_event():
+def test_eventbrite_event():
     eventbrite = Eventbrite()
     event = eventbrite.get_event(40146904472)
     assert event['id'] == '40146904472'
 
-def test_attendees():
+def test_eventbrite_attendees():
     eventbrite = Eventbrite()
     event = eventbrite.get_attendees(1059379633)
     assert len(event['attendees']) > 0
 
-def test_order():
+def test_eventbrite_order():
     eventbrite = Eventbrite()
     order = eventbrite.get_order(705451451)
     assert type(order) == dict
 
-def test_venue():
+def test_eventbrite_venue():
     eventbrite = Eventbrite()
     venue = eventbrite.get_venue(26449992)
     assert type(venue) == dict
+
+######################################
+# Tests for the Eventbrite dataloader
+######################################
+
+def test_eventbrite_data_loader():
+    eventbrite_loader = EventbriteLoader()
+    eventbrite_loader.run(test=True)
+
+def test_eventbrite_load_event():
+    eventbrite_loader = EventbriteLoader()
+    eventbrite_loader.database.delete_item('events', 'test_event')
+    event = eventbrite_loader.eventbrite.get_event(1059379633)
+    event['id'] = 'test_event'
+
+    eventbrite_loader.load_event(event)
+    test_event = eventbrite_loader.database.get_item('events', 'test_event')
+    assert test_event['id'] == 'test_event'
+    last_event_date = eventbrite_loader.database.last_event_date()
+    assert isinstance(last_event_date, datetime.datetime)
+
+    eventbrite_loader.database.delete_item('events', 'test_event')
+    test_event = eventbrite_loader.database.get_item('events', 'test_event')
+    assert test_event == None
+
+def test_eventbrite_load_attendee():
+    eventbrite_loader = EventbriteLoader()
+    eventbrite_loader.database.delete_item('attendees', 'test_attendee')
+    attendees = eventbrite_loader.eventbrite.get_attendees(1059379633)
+    attendee = attendees['attendees'][0]
+    attendee['id'] = 'test_attendee'
+
+    eventbrite_loader.load_attendee(attendee)
+    test_attendee = eventbrite_loader.database.get_item('attendees', 'test_attendee')
+    assert test_attendee['id'] == 'test_attendee'
+
+    eventbrite_loader.database.delete_item('attendees', 'test_attendee')
+    test_attendee = eventbrite_loader.database.get_item('attendees', 'test_attendee')
+    assert test_attendee == None
+
+def test_eventbrite_load_order():
+    eventbrite_loader = EventbriteLoader()
+    eventbrite_loader.database.delete_item('orders', 'test_order')
+    order = eventbrite_loader.eventbrite.get_order(23271451)
+    order['id'] = 'test_order'
+
+    eventbrite_loader.load_order(order)
+    test_order = eventbrite_loader.database.get_item('orders', 'test_order')
+    assert test_order['id'] == 'test_order'
+
+    eventbrite_loader.database.delete_item('orders', 'test_order')
+    test_order = eventbrite_loader.database.get_item('orders', 'test_order')
+    assert test_order == None

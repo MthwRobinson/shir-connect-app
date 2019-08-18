1d248b1d5f241d575e0b5fb340bb35482c2b04cd
matt <mthw.wm.robinson@gmail.com>
2019-08-18 13:16:40 -0400

    added function to find the bounding box of a number of points

--- ui/src/utilities/map.js
+++ ui/src/utilities/map.js
@@ -10,3 +10,41 @@ function getDefaultLocation() {
   return axios.get('/service/map/default');
 }
 export { getDefaultLocation }
+
+function getBoundingBox (data) {
+	// Gets a bounding box from a GeoJSON object of points
+	//
+	// Parameters:
+  // ----------
+	// data: object, the geojson object. needs a type key and a data key
+	//
+	// Returns:
+  // --------
+	// bounds: object, the bounds o fthe geojson object, with xMin, xMax,
+	// 	yMin, and yMax keys
+  var bounds = {}, coords, point, latitude, longitude;
+
+  // We want to use the “features” key of the FeatureCollection (see above)
+  data = data.features;
+
+  // Loop through each “feature”
+  for (var i = 0; i < data.length; i++) {
+
+    // Pull out the coordinates of this feature
+    coords = data[i].geometry.coordinates;
+		longitude = coords[0];
+		latitude = coords[1]
+
+		bounds.xMin = bounds.xMin < longitude ? bounds.xMin : longitude;
+		bounds.xMax = bounds.xMax > longitude ? bounds.xMax : longitude;
+		bounds.yMin = bounds.yMin < latitude ? bounds.yMin : latitude;
+		bounds.yMax = bounds.yMax > latitude ? bounds.yMax : latitude;
+
+  }
+
+  // Returns an object that contains the bounds of this GeoJSON
+  // data. The keys of this object describe a box formed by the
+  // northwest (xMin, yMin) and southeast (xMax, yMax) coordinates.
+  return bounds;
+}
+export { getBoundingBox }
